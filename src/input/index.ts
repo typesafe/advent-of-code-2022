5afd6fa0af82e57a74ae5599d451f4b683e1271b
Gino Heyman <gino.heyman@gmail.com>
2022-12-10 19:16:43 +0100

    day 3

---
+++ src/input/index.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+
+export class InputFile {
+  static async *readLines(path: string) {
+    const s = fs.createReadStream(path, { encoding: "utf-8" });
+    let buffer = "";
+
+    for await (const chunk of s) {
+      buffer += chunk;
+      const lines = buffer.split("\n");
+      buffer = lines.pop() ?? "";
+
+      for (const line of lines) {
+        yield line;
+      }
+    }
+    s.close();
+
+    if (buffer) {
+      yield buffer;
+    }
+  }
+}
